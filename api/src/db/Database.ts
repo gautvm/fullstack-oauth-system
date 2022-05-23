f37d05e1f5f37f1947f094f2fec8ceb5ce27134c
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2022-05-22 20:45:01 -0700

    set up boilerplate nextjs app

--- api/src/db/Database.ts
+++ api/src/db/Database.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"
 
 interface CustomNodeJsGlobal {
   prisma: PrismaClient;
---
+++ web/pages/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next/types";
+
+const Home: NextPage = () => {
+ return (
+   <div>
+     <h1>hi</h1>
+   </div>
+ )
+};
+
+export default Home;
