907397a12f65c83af9925acdc460109d3bd0e68a
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2022-06-07 10:46:04 -0700

    styled button.tsx

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -1,13 +1,18 @@
-export interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>, React.AriaAttributes {}
+export interface ButtonProps
+  extends React.DetailedHTMLProps<
+      React.ButtonHTMLAttributes<HTMLButtonElement>,
+      HTMLButtonElement
+    >,
+    React.AriaAttributes {}
 
-export const Button: React.FC<ButtonProps> = ({ children, ...props}) => {
-    return (
-        <button
-        type="button"
-        className="font-semibold border border-blue-500 bg-blue-500 text-white rounded-md px-3 py-1 m-2 transition duration-500 ease select-none hover:bg-blue-600 focus:outline-none focus:shadow-outline"
-        {...props}
-      >
-        {children}
-      </button>
-    )
-}
+export const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
+  return (
+    <button
+      type="button"
+      className="border border-blue-500 bg-blue-500 text-white rounded-md px-3 py-1 m-2 transition duration-500 ease select-none hover:bg-blue-600 focus:outline-none focus:shadow-outline"
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
