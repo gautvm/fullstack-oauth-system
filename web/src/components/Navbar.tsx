2eb2f18f2685b304a7e9f0d2fce93eb2eb7981c2
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2022-06-06 21:32:07 -0700

    get rid of arbitrary values

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -6,8 +6,8 @@ export const Navbar = () => {
   const user = useContext(UserContext) as UserEntity
     
   return (
-    <div className="flex items-center justify-between h-[4rem] px-6 py-4 border-b border-bgVariant1">
-      <div className="flex items-center justify-between h-[4rem] pb-1">
+    <div className="flex items-center justify-between h-16 px-6 py-4 border">
+      <div className="flex items-center justify-between h-16 pb-1">
         <div className="pt-1">
           <p className="font-semibold cursor-pointer">Company Logo</p>
         </div>
