79ebba65f0d84d226fdf46196157174e0564cf2a
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2022-06-08 15:36:12 -0700

    started to make a protected route system

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,37 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useAuth } from "./contexts/AuthContext";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
+
+const PrivateWrapper = ({ children }: { children: JSX.Element }) => {
+  const { user } = useAuth();
+  console.log(user)
+  return user ? children : <Navigate to="/login" />;
+};
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route
+    path="/private"
+    element={(
+      <PrivateWrapper>
+        <Private />
+      </PrivateWrapper>
+    )}
+  />
     </Routes>
   );
+}
+
+const Private = () => {
+  return (
+    <div>
+      this should be protected
+    </div>
+  )
 }
 
 export default App;
